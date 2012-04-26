5269f54211d0d8bfc96c9388b0a8d0c4a0bc413d
Chad Saunders <chad@ifrogz.com>
2012-04-26 10:54:26 -0600

    Making Amazon_SQS compatible with Kohana 3.x
    Changed a couple Kohana::config() to Kohana::$config->load()

--- classes/amazon/sqs.php
+++ classes/amazon/sqs.php
@@ -6,7 +6,7 @@ class Amazon_SQS extends SQS
 {
 	public function __construct() 
 	{
-		parent::__construct(Kohana::config('amazon.account.access_key'), Kohana::config('amazon.account.secret_key'));
+		parent::__construct(Kohana::$config->load('amazon.account.access_key'), Kohana::$config->load('amazon.account.secret_key'));
 	}
 }
 
